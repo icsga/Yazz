0f18b189e8215be817fec6b0894958ade41fb1b4
ICS <isnospam-bitbucket@yahoo.de>
2019-11-09 15:32:35 +0900

    Removed moved files.

--- src/sample_generator.rs
+++
@@ -1,8 +0,0 @@
-use super::Float;
-use super::SoundData;
-
-pub trait SampleGenerator {
-    fn get_sample(&mut self, frequency: Float, sample_clock: i64, data: &SoundData, reset: bool) -> (Float, bool);
-    fn reset(&mut self, sample_clock: i64);
-}
-
