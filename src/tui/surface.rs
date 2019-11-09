fc430f8ba96c93044f61de6249431b7f415600ea
ICS <isnospam-bitbucket@yahoo.de>
2019-11-09 19:59:40 +0900

    First three controls are working.

--- src/tui/surface.rs
+++ src/tui/surface.rs
@@ -21,10 +21,18 @@ impl Surface {
         let mod_targets = HashMap::new();
         let mut this = Surface{window, controller, mod_targets};
         let mod_targets: HashMap<ParamId, ObserverRef> = HashMap::new();
-        let key = ParamId{function: Parameter::Oscillator, function_id: 1, parameter: Parameter::Level};
+        let mut key = ParamId{function: Parameter::Oscillator, function_id: 1, parameter: Parameter::Level};
 
-        let osc1_level = this.new_mod_dial_float("Level", 0.0, 100.0, 0.0, key);
+        let osc1_level = this.new_mod_dial_float("Level", 0.0, 100.0, 0.0, &key);
         this.window.add_child(osc1_level, 1, 1);
+
+        Surface::update_key(&mut key, Parameter::Oscillator, 1, Parameter::Phase);
+        let osc1_phase = this.new_mod_dial_float("Phase", 0.0, 1.0, 0.0, &key);
+        this.window.add_child(osc1_phase, 1, 4);
+
+        Surface::update_key(&mut key, Parameter::Oscillator, 1, Parameter::Blend);
+        let osc1_blend = this.new_mod_dial_float("Phase", 0.0, 5.0, 0.0, &key);
+        this.window.add_child(osc1_blend, 1, 7);
 
         this.window.set_position(1, 1);
         this
@@ -38,21 +46,27 @@ impl Surface {
         self.window.draw();
     }
 
-    pub fn update_value(&mut self, key: ParamId, value: Value) {
+    pub fn update_value(&mut self, key: &ParamId, value: Value) {
         self.controller.update(key, value);
     }
 
-    fn new_mod_dial_float(&mut self, label: &'static str, min: f64, max: f64, value: f64, key: ParamId) -> ContainerRef {
+    fn new_mod_dial_float(&mut self, label: &'static str, min: f64, max: f64, value: f64, key: &ParamId) -> ContainerRef {
         let mut c = Container::new(19, 3);
-        let label = Label::new(label);
+        let label = Label::new(label, 10);
         let dial = Dial::new(Value::Float(min), Value::Float(max), Value::Float(value));
         let modul = Bar::new(Value::Float(0.0), Value::Float(100.0), Value::Float(0.0));
         self.controller.add_observer(key, dial.clone());
-        self.mod_targets.insert(key, modul.clone());
+        self.mod_targets.insert(*key, modul.clone());
         c.add_child(label, 1, 1);
-        c.add_child(dial, 18, 1);
+        c.add_child(dial, 12, 1);
         c.add_child(modul, 1, 2);
         Rc::new(RefCell::new(c))
+    }
+
+    fn update_key(key: &mut ParamId, func: Parameter, func_id: usize, param: Parameter) {
+        key.function = func;
+        key.function_id = func_id;
+        key.parameter = param;
     }
 }
 
